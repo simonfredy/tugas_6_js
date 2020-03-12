6dbfbe2e040d0ca7bceb4278c755561b08aa6984
Simon Fredy Sinaga <simonfredysinaga@gmail.com>
2020-03-12 18:12:48 +0700

    Tugas 6 JS

---
+++ tugas6.js
@@ -0,0 +1,12 @@
+function tugasEnam(){
+    for (i = 0; i <= 20; i++){
+        j = i % 2;
+        if (j == 0){
+            console.log("Bilangan Genap");
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+tugasEnam();
